908f620ce2f0d72e9c48e9c410e0c3ac5e111065
Zax <zxhx.chan@yahoo.com>
2020-05-11 23:10:40 +0800

    Update matching2D_Student.cpp

--- src/matching2D_Student.cpp
+++ src/matching2D_Student.cpp
@@ -218,11 +218,11 @@ void detKeypointsHarris(std::vector<cv::KeyPoint> &keypoints, cv::Mat &img, bool
 void detKeypointsModern(vector<cv::KeyPoint> &keypoints, cv::Mat &img, std::string detectorType, bool bVis) {
     // select appropriate descriptor
     cv::Ptr<cv::FeatureDetector> detector;
-    if (detectorType.compare("BRIEF") == 0) {
+    if (detectorType.compare("BRISK") == 0) {
         int threshold = 30;        // FAST/AGAST detection threshold score.
         int octaves = 3;           // detection octaves (use 0 to do single scale)
         float patternScale = 1.0f; // apply this scale to the pattern used for sampling the neighbourhood of a keypoint.
-        detector = cv::BRIEF::create(threshold, octaves, patternScale);
+        detector = cv::BRISK::create(threshold, octaves, patternScale);
 
     } else if (detectorType.compare("AKAZE") == 0) {
         detector = cv::AKAZE::create();
@@ -238,7 +238,7 @@ void detKeypointsModern(vector<cv::KeyPoint> &keypoints, cv::Mat &img, std::stri
     } else if (detectorType.compare("SIFT") == 0) {
         detector = cv::xfeatures2d::SIFT::create();
     } else {
-        throw invalid_argument(detectorType + " is not supported, FAST, BRIEF, ORB, AKAZE, SIFT are valid detectorTypes");
+        throw invalid_argument(detectorType + " is not supported, FAST, BRISK, ORB, AKAZE, SIFT are valid detectorTypes");
     }
 
     // perform feature description
