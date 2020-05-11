e1e7033b14e0ba723c8c9365ed6aac4cc4623663
Zax <zxhx.chan@yahoo.com>
2020-05-11 23:12:48 +0800

    Update matching2D.hpp

--- src/matching2D.hpp
+++ src/matching2D.hpp
@@ -19,75 +19,12 @@
 #include "dataStructures.h"
 
 
-
+void detKeypointsHarris(std::vector<cv::KeyPoint> &keypoints, cv::Mat &img, bool bVis=false);
 void detKeypointsShiTomasi(std::vector<cv::KeyPoint> &keypoints, cv::Mat &img, bool bVis=false);
 void detKeypointsModern(std::vector<cv::KeyPoint> &keypoints, cv::Mat &img, std::string detectorType, bool bVis=false);
 void descKeypoints(std::vector<cv::KeyPoint> &keypoints, cv::Mat &img, cv::Mat &descriptors, std::string descriptorType);
 void matchDescriptors(std::vector<cv::KeyPoint> &kPtsSource, std::vector<cv::KeyPoint> &kPtsRef, cv::Mat &descSource, cv::Mat &descRef,
-                      std::vector<cv::DMatch> &matches, std::string descriptorType, std::string matcherType, std::string selectorType);
+                      std::vector<cv::DMatch> &matches, std::string descriptorclass, std::string matcherType, std::string selectorType);
 
-// declare type: keypionts detector
-void detKeypointsHarris(std::vector<cv::KeyPoint> &keypoints, cv::Mat &img, bool bVis=false);
-void detKeypointsFast(std::vector<cv::KeyPoint> &keypoints, cv::Mat &img, bool bVis=false);
-void detKeypointsBrisk(std::vector<cv::KeyPoint> &keypoints, cv::Mat &img,bool bVis=false);
-void detKeypointsOrb(std::vector<cv::KeyPoint> &keypoints, cv::Mat &img,bool bVis=false);
-void detKeypointsAkaze(std::vector<cv::KeyPoint> &keypoints, cv::Mat &img, bool bVis=false);
-void detKeypointsSift(std::vector<cv::KeyPoint> &keypoints, cv::Mat &img, bool bVis=false);
-
-// declare type: descriptor
-void descKeypointsOtherType(cv::Ptr<cv::DescriptorExtractor>& extractor,std::vector<cv::KeyPoint> &keypoints, 
-cv::Mat &img, cv::Mat &descriptors, std::string descriptorType);
-void descKeypointsBrisk(cv::Ptr<cv::DescriptorExtractor>& extractor,std::vector<cv::KeyPoint> &keypoints, 
-cv::Mat &img, cv::Mat &descriptors);
-void descKeypointsBrief(cv::Ptr<cv::DescriptorExtractor>& extractor,std::vector<cv::KeyPoint> &keypoints, 
-cv::Mat &img, cv::Mat &descriptors);
-void descKeypointsOrb(cv::Ptr<cv::DescriptorExtractor>& extractor, std::vector<cv::KeyPoint> &keypoints, 
-cv::Mat &img, cv::Mat &descriptors);
-void descKeypointsFreak(cv::Ptr<cv::DescriptorExtractor>& extractor,std::vector<cv::KeyPoint> &keypoints, 
-cv::Mat &img, cv::Mat &descriptors);
-void descKeypointsAkaze(cv::Ptr<cv::DescriptorExtractor>& extractor,std::vector<cv::KeyPoint> &keypoints, 
-cv::Mat &img, cv::Mat &descriptors);
-void descKeypointsSift(cv::Ptr<cv::DescriptorExtractor>& extractor,std::vector<cv::KeyPoint> &keypoints, 
-cv::Mat &img,cv::Mat &descriptors);
-
-// declare class: spreadMap
-class spreadMap
-{
-    private:
-        std::vector<std::string> fieldVector;
-        std::vector<unsigned int> rowVector;
-        std::vector<std::vector<float>> vlaue;
-
-    public:
-        spreadMap(std::vector<unsigned int> rowVector,std::vector<std::string> fieldVector)
-        {
-            this->rowVector = rowVector;
-            this->fieldVector = fieldVector;
-            int row = rowVector.size();
-            int col = fieldVector.size();
-            for (int i=0;i<row;i++)
-            {
-                std::vector<float> rowValue(col,0.0);
-                this->vlaue.push_back(rowValue);
-            }
-        }
-        ~spreadMap(){}
-        void setValue(int row,std::string field, float t)
-        {
-            std::vector<std::string>::iterator iter;
-            iter = std::find(this->fieldVector.begin(),this->fieldVector.end(),field);
-            int col = iter - this->fieldVector.begin();
-            this->vlaue[row][col] = t;
-        }
-        
-        float getValue(int row, std::string field)
-        {
-            std::vector<std::string>::iterator iter;
-            iter = std::find(this->fieldVector.begin(),this->fieldVector.end(),field);
-            int col = iter - this->fieldVector.begin();
-            return this->vlaue[row][col];
-        }
-
-};
-
-#endif /* matching2D_hpp */
+#endif
+// matching2D_hpp
